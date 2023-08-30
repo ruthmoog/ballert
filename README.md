95d2d0c05496ecd60f0c8b5ade47a787cd0d2fd8
Ruth Baker <ruthmoog@gmail.com>
2023-08-30 11:30:58 +0100

    Create empty app with tests

---
+++ README.md
@@ -0,0 +1,11 @@
+# ballert
+
+This project is an alert tool that will check the status of ballet class availability changes for the 
+[Dance With the Royal Ballet](https://www.roh.org.uk/tickets-and-events/dance-with-the-royal-ballet-dates) season from 29 Sept 2023 - 3 Nov 2023.
+
+## Alerts
+
+Tests will be run automatically TODO
+
+- To run test manually, use `npx playwright test`, or `npx playwright test --ui` for UI mode
+- Edit test behaviour in `tests/example.spec.js`
---
+++ tests/example.spec.js
@@ -0,0 +1,16 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('a hello world check that we have the right page', async ({ page }) => {
+  await page.goto('https://www.roh.org.uk/tickets-and-events/dance-with-the-royal-ballet-dates');
+  await expect(page).toHaveTitle(/Dance with The Royal Ballet/);
+});
+
+test('check for sold out items', async ({ page }) => {
+  await page.goto('https://www.roh.org.uk/tickets-and-events/dance-with-the-royal-ballet-dates');
+  const soldOutItems = await page.getByRole('heading', { name: 'Sold out' });
+
+  // Expects page to have three Sold out items.
+  // If there are not 3 sold out items, there may be returns for sale!
+  await expect(soldOutItems).toHaveCount(3);
+});
