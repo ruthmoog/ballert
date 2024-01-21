41409262ef30d811e6db35ef43a2059918c5733f
Ruth Baker <33294286+ruthmoog@users.noreply.github.com>
2024-01-21 21:12:26 +0000

    Update tests for Maz & Izu

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,16 +1,24 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('a hello world check that we have the right page', async ({ page }) => {
-  await page.goto('https://www.roh.org.uk/tickets-and-events/dance-with-the-royal-ballet-dates');
+test('check for Izumis Friday classes', async ({ page }) => {
+  await page.goto('https://www.roh.org.uk/tickets-and-events/dance-with-the-royal-ballet-dates?start-date=01-03-24&end-date=31-12-24');
+
+  // Check we're on the right page
   await expect(page).toHaveTitle(/Dance with The Royal Ballet/);
+
+  // If there are not 3 sold out items, there may be returns for sale or new dates!
+  const soldOutItems = await page.getByRole('heading', { name: 'Sold out' });
+  await expect(soldOutItems).toHaveCount(3);
 });
 
-test('check for sold out items', async ({ page }) => {
-  await page.goto('https://www.roh.org.uk/tickets-and-events/dance-with-the-royal-ballet-dates');
-  const soldOutItems = await page.getByRole('heading', { name: 'Sold out' });
+test('check for Mazs Family Sunday', async ({ page }) => {
+  await page.goto('https://www.roh.org.uk/tickets-and-events/family-sundays-dates?start-date=26-05-24&end-date=26-05-24&event-time=afternoon');
 
-  // Expects page to have three Sold out items.
-  // If there are not 4 sold out items, there may be returns for sale!
-  await expect(soldOutItems).toHaveCount(4);
+  // Check we're on the right page
+  await expect(page).toHaveTitle(/Family Sundays/);
+  
+    // If there are no sold out items, there may be returns for sale!
+  const soldOutItems = await page.getByRole('heading', { name: 'Sold out' });
+  await expect(soldOutItems).toHaveCount(1);
 });
