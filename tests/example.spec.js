0db3fd2d3c8849489599e5aba71b45d673f7612c
Ruth Baker <33294286+ruthmoog@users.noreply.github.com>
2024-05-22 14:31:38 +0100

    err, I should probably have fixed this ...

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,27 +1,27 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('check for Izumis Friday classes', async ({ page }) => {
-  await page.goto('https://www.roh.org.uk/tickets-and-events/dance-with-the-royal-ballet-dates?start-date=01-03-24&end-date=31-12-24');
+// test('check for Izumis Friday classes', async ({ page }) => {
+//   await page.goto('https://www.roh.org.uk/tickets-and-events/dance-with-the-royal-ballet-dates?start-date=01-03-24&end-date=31-12-24');
 
-  // Check we're on the right page
-  await expect(page).toHaveTitle(/Dance with The Royal Ballet/);
+//   // Check we're on the right page
+//   await expect(page).toHaveTitle(/Dance with The Royal Ballet/);
 
-  // If there are not 3 sold out items, there may be returns for sale or new dates!
-  const soldOutItems = await page.getByRole('heading', { name: 'Sold out' });
-  await expect(soldOutItems).toHaveCount(2);
-});
+//   // If there are not 3 sold out items, there may be returns for sale or new dates!
+//   const soldOutItems = await page.getByRole('heading', { name: 'Sold out' });
+//   await expect(soldOutItems).toHaveCount(2);
+// });
 
-test('check for Mazs Family Sunday', async ({ page }) => {
-  await page.goto('https://www.roh.org.uk/tickets-and-events/family-sundays-dates?start-date=26-05-24&end-date=26-05-24&event-time=afternoon');
+// test('check for Mazs Family Sunday', async ({ page }) => {
+//   await page.goto('https://www.roh.org.uk/tickets-and-events/family-sundays-dates?start-date=26-05-24&end-date=26-05-24&event-time=afternoon');
 
-  // Check we're on the right page
-  await expect(page).toHaveTitle(/Family Sundays/);
+//   // Check we're on the right page
+//   await expect(page).toHaveTitle(/Family Sundays/);
   
-    // If there are no sold out items, there may be returns for sale!
-  const soldOutItems = await page.getByRole('heading', { name: 'Sold out' });
-  await expect(soldOutItems).toHaveCount(1);
-});
+//     // If there are no sold out items, there may be returns for sale!
+//   const soldOutItems = await page.getByRole('heading', { name: 'Sold out' });
+//   await expect(soldOutItems).toHaveCount(1);
+// });
 
 test('check for bod discounts', async ({page}) => {
   await page.goto('https://www.knightsportswear.com/collections/bowmen-of-darentford/products/bowmen-of-darenteford-white-tech-polo-right');
