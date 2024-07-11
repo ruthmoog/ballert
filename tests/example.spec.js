d057b819a29f487e061b8c575dea938e1be6ec60
Ruth Baker <33294286+ruthmoog@users.noreply.github.com>
2024-07-11 12:57:58 +0100

    skip all tests except new dance with royal ballet dates

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,16 +1,16 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-// test('check for Izumis Friday classes', async ({ page }) => {
-//   await page.goto('https://www.roh.org.uk/tickets-and-events/dance-with-the-royal-ballet-dates?start-date=01-03-24&end-date=31-12-24');
+test('check for Izumis Friday classes', async ({ page }) => {
+  await page.goto('https://www.rbo.org.uk/tickets-and-events/dance-with-the-royal-ballet-dates?start-date=11-10-24&end-date=08-11-24');
 
-//   // Check we're on the right page
-//   await expect(page).toHaveTitle(/Dance with The Royal Ballet/);
+  // Check we're on the right page
+  await expect(page).toHaveTitle(/Dance with The Royal Ballet/);
 
-//   // If there are not 3 sold out items, there may be returns for sale or new dates!
-//   const soldOutItems = await page.getByRole('heading', { name: 'Sold out' });
-//   await expect(soldOutItems).toHaveCount(2);
-// });
+  // If there are not 3 sold out items, there may be returns for sale or new dates!
+  const soldOutItems = await page.getByRole('heading', { name: 'Sold out' });
+  await expect(soldOutItems).toHaveCount(3);
+});
 
 // test('check for Mazs Family Sunday', async ({ page }) => {
 //   await page.goto('https://www.roh.org.uk/tickets-and-events/family-sundays-dates?start-date=26-05-24&end-date=26-05-24&event-time=afternoon');
@@ -23,10 +23,10 @@ const { test, expect } = require('@playwright/test');
 //   await expect(soldOutItems).toHaveCount(1);
 // });
 
-test('check for bod discounts', async ({page}) => {
-  await page.goto('https://www.knightsportswear.com/collections/bowmen-of-darentford/products/bowmen-of-darenteford-white-tech-polo-right');
+// test('check for bod discounts', async ({page}) => {
+//   await page.goto('https://www.knightsportswear.com/collections/bowmen-of-darentford/products/bowmen-of-darenteford-white-tech-polo-right');
 
-  // If the price has changed, it might be on sale!
-  const price = await page.getByRole('heading', { name: '£34.95' });
-  await expect(price).toHaveCount(1);
-});
+//   // If the price has changed, it might be on sale!
+//   const price = await page.getByRole('heading', { name: '£34.95' });
+//   await expect(price).toHaveCount(1);
+// });
